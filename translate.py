fc99d1388b312ed597c14a2fe30960b2e650f910
Fabian Petras <34797639+fpetras@users.noreply.github.com>
2019-01-24 21:07:33 +0100

    Add main function

--- translate.py
+++ translate.py
@@ -8,16 +8,24 @@ from print_languages import decode, print_languages, print_language_name
 
 def translate_text(text, target_language):
 	client = translate.Client()
+	confidence = client.detect_language(text)
 	result = client.translate(text, target_language)
 
+	print('Detected language confidence: '),
+	print(confidence['confidence'])
 	print_language_name(result['detectedSourceLanguage'])
 	print(result['input'])
 	print_language_name(target_language)
 	print(decode(result['translatedText']))
 
-if len(sys.argv) != 3:
-	print('usage: ./translate.py "Text to translate" "Target language"')
-	print_languages()
-	sys.exit()
-lang = str_to_iso_639_1(sys.argv[2])
-translate_text(sys.argv[1], lang)
+
+def main(argv):
+	if len(sys.argv) != 3:
+		print('usage: ./translate.py "Text to translate" "Target language"')
+		print_languages()
+		sys.exit()
+	lang = str_to_iso_639_1(sys.argv[2])
+	translate_text(sys.argv[1], lang)
+
+if __name__ == "__main__":
+	main(sys.argv)
