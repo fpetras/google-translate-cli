c2e42655fafc16f70ed85c3927cec4e6e6ced1a1
Fabian Petras <34797639+fpetras@users.noreply.github.com>
2019-01-25 12:53:19 +0100

    Add helpers.py

---
+++ helpers.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+def credentials():
+	print('''\
+┌──────────────────────────────────────────────────────┐
+│   You need a credentials file to use this version    │
+│   Refer to README.md or instructions.txt             │
+│   Or use the other version that doesn't need it      │
+└──────────────────────────────────────────────────────┘''')
+    	
+
+def print_usage():
+	print('''\
+usage: ./translate.py [options] [Input to translate] [target language [...]]
+
+optional arguments:
+  -h, --help  show this help message and exit
+  -f FILE     translate FILE
+  -i          interactive mode''')
+
+def valid_lang(lang):
+	if lang == False:
+		return False
+	return True
